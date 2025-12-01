d37af339669b048dc93d09e760c2d3668bbac267
Robert Lewis <Robbysim37@gmail.com>
2025-12-01 08:22:52 -0500

    updated backend to allow frontend API calls

--- RPSBackend/Program.cs
+++ RPSBackend/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddCors(options =>
         policy
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .WithOrigins("http://localhost:5173"); // your frontend dev server
+            .WithOrigins("http://localhost:5173","https://rps-frontend-1dzl.vercel.app/");
     });
 });
 
